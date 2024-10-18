1f2921dba8302546e308951dc09c4f3a5f3e444a
Alina Meyer <alina.eichmatt@gmail.com>
2024-10-18 18:04:21 +0200

    Save obstacles seperatly from graph

--- simulator/robot.py
+++ simulator/robot.py
@@ -27,19 +27,22 @@ class Robot:
     # e.g. from left to right for robot to turn & check in a way that makes sense
     def check_next_nodes(self):
         obstacles = self.graph_reader.read_obstacles()
-        neighbors = obstacles[self.current_node]
+        neighbors = self.get_neighbors()
+        print(f"[ET] I am now infront of my next node {self.current_node}. I will now check for the next {len(neighbors)} connections.")
 
         for neighbor in neighbors:
-            for node, edge in neighbor.items():
-                if isinstance(edge, list) and 'O' in edge:
-                    print(f"There is a movable object between node {self.current_node} and {node}.")
-                    self.increase_weight_for_edge_with_obstacle(node)
-                if "P" in obstacles[node]:
-                    print(f"Pylon on {node}.")
-                    self.remove_node(node)
+            check_connections = {neighbor, self.current_node}
+            if any(check_connections.issubset(set(sublist)) for sublist in obstacles["barrier"]):
+                print(f"There is a movable object between node {self.current_node} and {neighbor}.")
+                self.increase_weight_for_edge_with_obstacle(neighbor)
+            if any(check_connections.issubset(set(sublist)) for sublist in obstacles["missing_line"]):
+                print(f"There is a missing line between connection {self.current_node} and {neighbor}.")
+                self.remove_edge(neighbor)
+            if neighbor in obstacles["cone"]:
+                print(f"Pylon on {neighbor}.")
+                self.remove_node(neighbor)
 
     def increase_weight_for_edge_with_obstacle(self, node):
-        # TODO: I am sure this could be done better
         self.graph[self.current_node] = [{k: v + OBJECT_WEIGHT if k == node else v for k, v in neighbor.items()} for neighbor in self.graph[self.current_node]]
         self.graph[node] = [{k: v + OBJECT_WEIGHT if k == self.current_node else v for k, v in neighbor.items()} for neighbor in self.graph[node]]
         print(f"Weight between {self.current_node} and {node} has been raised by {OBJECT_WEIGHT}.")
@@ -50,3 +53,12 @@ class Robot:
             edges = [d for d in edges if node_to_remove not in d]
             self.graph[node] = edges
         print(f"Removed node {node_to_remove} including all its edges.")
+
+    def remove_edge(self, neighbor):
+        self.graph[self.current_node] = [item for item in self.graph[self.current_node] if not (isinstance(item, dict) and neighbor in item)]
+        self.graph[neighbor] = [item for item in self.graph[neighbor] if not (isinstance(item, dict) and self.current_node in item)]
+        print(f"Removed edge between {neighbor} and {self.current_node}.")
+
+    def get_neighbors(self):
+        neighboring_nodes = self.graph[self.current_node]
+        return [list(n.keys())[0] for n in neighboring_nodes]
